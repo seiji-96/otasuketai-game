a2a88a98dafa07a94d684493e2269f98acb41d6e
seiji-96 <seiji@uniba.jp>
2022-08-25 18:08:10 +0900

    う

--- My project/Assets/Scripts/Enemyspawn.cs
+++ My project/Assets/Scripts/Enemyspawn.cs
@@ -4,19 +4,14 @@ using UnityEngine;
 
 public class Enemyspawn : MonoBehaviour
 {
-    public List<GameObject> enemy;
+    public GameObject enemy;
 
-    GameObject[] step = new GameObject[2];
+    GameObject[] step = new GameObject[5];
     public float speed = 10;
     private float disappear = -10;
-    private float respawn = 30;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < step.Length; i++)
-        {
-            step[i] = Instantiate(enemy[i], new Vector3(12 * i, 1, 0), Quaternion.identity);
-        }
     }
 
     // Update is called once per frame
@@ -24,11 +19,14 @@ public class Enemyspawn : MonoBehaviour
     {
         for (int i = 0; i < step.Length; i++)
         {
+            int n = Random.Range(1, 4);
+            step[i] = Instantiate(enemy, new Vector3(20f, 0.25f, 0.5f * n), Quaternion.identity);
             step[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
             if (step[i].gameObject.transform.position.x < disappear)
             {
-                step[i].gameObject.transform.position = new Vector3(respawn, 1, 0);
+                Destroy(step[i].gameObject);
             }
         }
     }
+
 }
