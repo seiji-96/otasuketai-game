4a6b85fcc3128681510f8918b4bc2dd824e978b9
arata83 <arata@uniba.jp>
2022-08-25 18:32:31 +0900

    title

---
+++ My project/Assets/Scripts/StartTimer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartTimer : MonoBehaviour
+{
+    public Text timerText;
+
+	public float totalTime;
+	int seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        totalTime -= Time.deltaTime;
+		seconds = (int)totalTime;
+		timerText.text= seconds.ToString();
+        if (totalTime<=0 && totalTime>=-0.5)
+        {
+            timerText.text = "Start";
+        }
+        if (totalTime<-0.5)
+        {
+            timerText.text = "";
+        }
+    }
+}
