095113f751babc228d82cd3fdb7caabbca8c1e01
arata83 <arata@uniba.jp>
2022-11-07 19:06:31 +0900

    星の遠近感

--- My project/Assets/Scripts/StarSyori.cs
+++ My project/Assets/Scripts/StarSyori.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class StarSyori : MonoBehaviour
 {
     public List<GameObject> stage;
+    private GameObject timerObject;
+    StartTimer timerScript;
 
     GameObject[] step = new GameObject[10];
 
-    public float speed = 3;
+    public float speed;
     private float disappear = -10;
     private float respawn = 30;
 
     // Start is called before the first frame update
     void Start()
     {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        timerObject = GameObject.Find("GameObject");
+        timerScript = timerObject.GetComponent<StartTimer>();
         for (int i = 0; i < step.Length; i++)
         {
-            step[i] = Instantiate(stage[i], new Vector3(4 * i, 10 * (-1)^i, 3), Quaternion.identity);
+            step[i] = Instantiate(stage[i], new Vector3(100 * (i+1) , (-1) * Random.Range(1, 200) , Random.Range(80, 100) * Mathf.Pow(-1, i)), Quaternion.identity);
         }
     }
 
@@ -29,13 +34,16 @@ public class StarSyori : MonoBehaviour
 
     void FixedUpdate()
     {
-        for (int i = 0; i < step.Length; i++)
+        if (timerScript.totalTime<=0)
         {
-            speed = 1.000001f * speed;
-            step[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
-            if (step[i].gameObject.transform.position.x < disappear)
+            for (int i = 0; i < step.Length; i++)
             {
-                step[i].gameObject.transform.position = new Vector3(respawn, 0, 0);
+                speed = 1.000001f * speed;
+                step[i].gameObject.transform.position -= new Vector3(speed * Time.deltaTime, 0, 0);
+                if (step[i].gameObject.transform.position.x < disappear)
+                {
+                    step[i].gameObject.transform.position = new Vector3(respawn, 0, 0);
+                }
             }
         }
     }
